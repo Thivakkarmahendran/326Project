e064c349ee2f218387f4502fb53458c7982eacb1
Thivakkar Mahendran <mthivakkar@gmail.com>
2019-11-15 22:33:32 -0500

    Get frame from live camera feed

--- mainScreen.js
+++ mainScreen.js
@@ -3,20 +3,18 @@ var video = document.getElementById('video');
 
 // Get access to the camera!
 if(navigator.mediaDevices && navigator.mediaDevices.getUserMedia) {
-    // Not adding `{ audio: true }` since we only want video now
     navigator.mediaDevices.getUserMedia({ video: true }).then(function(stream) {
-        //video.src = window.URL.createObjectURL(stream);
         video.srcObject = stream;
         video.play();
     });
 }
 
-// Elements for taking the snapshot
+// Elements for getting frame
 var canvas = document.getElementById('canvas');
 var context = canvas.getContext('2d');
 var video = document.getElementById('video');
 
-// Trigger photo take
-document.getElementById("snap").addEventListener("click", function() {
+// get frame
+document.getElementById("getFrame").addEventListener("click", function() {
 	context.drawImage(video, 0, 0, 640, 480);
 });
