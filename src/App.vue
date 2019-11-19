c555cbf0e5f6f3f2852cadfe8e3ab202a239fae0
Andrew Smith <andy-smith@comcast.net>
2019-11-19 12:32:33 -0500

    Fixed template issues
    Vue template reverted to original, edited to actually reflect the about page

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,17 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <About/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import About from './components/About.vue'
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    About
   }
 }
 </script>
---
+++ src/components/About.vue
@@ -0,0 +1,43 @@
+<template>
+    <div class="about">
+        <h1>Welcome to the 326 Facial Recognition App</h1>
+        <h3>Github Source</h3>
+        <ul>
+            <li><a href="https://github.com/Thivakkarmahendran/326Project" target="_blank" rel="noopener">326 Source Code</a></li>
+        </ul>
+        <h3>Authors</h3>
+        <ul>
+            <li><a href="https://github.com/Thivakkarmahendran" target="_blank" rel="noopener">Thivakkar Mahendran</a></li>
+            <li><a href="https://github.com/akhilraheja" target="_blank" rel="noopener">Akhil Raheja</a></li>
+            <li><a href="https://github.com/Smith1255" target="_blank" rel="noopener">Andrew Smith</a></li>
+            <li><a href="https://github.com/Rjambunathan" target="_blank" rel="noopener">Rishi Jambunathan</a></li>
+        </ul>
+        <h3>Ecosystem</h3>
+        <ul>
+            <li><a href="https://vuejs.org" target="_blank" rel="noopener">vue</a></li>
+        </ul>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "About"
+    }
+</script>
+
+<style scoped>
+    h3 {
+        margin: 40px 0 0;
+    }
+    ul {
+        list-style-type: none;
+        padding: 0;
+    }
+    li {
+        display: inline-block;
+        margin: 0 10px;
+    }
+    a {
+        color: #42b983;
+    }
+</style>
