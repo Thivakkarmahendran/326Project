1e7ff2a9d4c186ee54bc395a6f8c10923dfdb0a5
Andrew Smith <andy-smith@comcast.net>
2019-12-04 17:40:22 -0500

    Update not working
    Update

---
+++ node_modules/tfjs-image-recognition-base/karma.conf.js
@@ -0,0 +1,36 @@
+const dataFiles = [
+  'test/*.png',
+  'test/*.json',
+  'test/*.weights'
+].map(pattern => ({
+  pattern,
+  watched: false,
+  included: false,
+  served: true,
+  nocache: false
+}))
+
+module.exports = function(config) {
+  config.set({
+    frameworks: ['jasmine', 'karma-typescript'],
+    files: [
+      'src/**/*.ts',
+      'test/**/*.ts'
+    ].concat(dataFiles),
+    exclude: ['test/env.node.ts'],
+    preprocessors: {
+      '**/*.ts': ['karma-typescript']
+    },
+    karmaTypescriptConfig: {
+      tsconfig: 'tsconfig.test.json'
+    },
+    browsers: ['Chrome'],
+    browserNoActivityTimeout: 120000,
+    captureTimeout: 60000,
+    client: {
+      jasmine: {
+        timeoutInterval: 60000
+      }
+    }
+  })
+}
--- src/components/Enrollment.vue
+++ src/components/Enrollment.vue
@@ -1,6 +1,7 @@
 <template>
     <div>
         <video muted id="video" width="100%" height="100%" autoplay></video>
+        <canvas id="frame" width="640" height="480"></canvas>
     </div>
 </template>
 
@@ -10,11 +11,13 @@
         name: 'Enrollment'
     }
     // Prefer camera resolution nearest to 1280x720.
+    var video;
     var constraints = { audio: false, video: { width: 1280, height: 720 } };
+
 
     navigator.mediaDevices.getUserMedia(constraints)
         .then(function(mediaStream) {
-            var video = document.querySelector('video');
+            video = document.querySelector('video');
             video.srcObject = mediaStream;
             video.onloadedmetadata = function() {
                 video.play();
@@ -22,6 +25,45 @@
         })
         .catch(function(err) { console.log(err.name + ": " + err.message); }); // always check for errors at the end.
 
+    //---------------------
+    // TAKE A SNAPSHOT CODE
+    //---------------------
+
+    // implements nodejs wrappers for HTMLCanvasElement, HTMLImageElement, ImageData
+
+    import * as canvas from 'canvas';
+
+    import * as faceapi from 'face-api.js';
+    // // patch nodejs environment, we need to provide an implementation of
+    // // HTMLCanvasElement and HTMLImageElement, additionally an implementation
+    // // of ImageData is required, in case you want to use the MTCNN
+
+    var frame, ctx;
+    const { Canvas, Image, ImageData } = canvas;
+    const MODEL_URL = './assets/models';
+
+    faceapi.env.monkeyPatch({ Canvas, Image, ImageData });
+
+
+    window.setInterval(function(){
+       snapshot();
+    }, 1000);
+
+    async function snapshot() {
+        frame = document.getElementById("frame");
+        ctx = frame.getContext('2d');
+        // Draws current image from the video element into the canvas
+        ctx.drawImage(video, 0,0, frame.width, frame.height);
+
+        await faceDetectionNet.loadFromDisk('../../weights');
+        await faceapi.nets.faceLandmark68Net.loadFromDisk('../../weights');
+        await faceapi.nets.faceRecognitionNet.loadFromDisk('../../weights');
+
+        const resultsRef = await faceapi.detectAllFaces(frame, faceDetectionOptions)
+            .withFaceLandmarks()
+            .withFaceDescriptors()
+
+    }
 </script>
 
 <style scoped>
