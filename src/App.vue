40f71ad454cb38adff5e7cde81f3dfbd2fc00dc6
Andrew Smith <andy-smith@comcast.net>
2019-12-05 13:10:41 -0500

    test
    test

--- src/App.vue
+++ src/App.vue
@@ -21,8 +21,8 @@ export default {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
+  /*text-align: center;*/
   color: #2c3e50;
-  margin-top: 0;
+  /*margin-top: 0;*/
 }
 </style>
