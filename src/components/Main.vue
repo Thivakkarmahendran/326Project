b5d15e6418846bf5411b973a0e59df3f4b2a50a9
Andrew Smith <andy-smith@comcast.net>
2019-12-08 17:53:39 -0500

    Deleted old code

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -39,15 +39,16 @@
                 }
 
                 video.addEventListener('play', async () => {
-                    let container = document.getElementById("mainComponent");
                     const labeledFaceDescriptors = await loadLabeledImages();
+                    let container = document.getElementById("mainComponent");
                     const faceMatcher = new faceapi.FaceMatcher(labeledFaceDescriptors, 0.6);
                     let canvas;
-                    canvas = faceapi.createCanvasFromMedia(video);
-                    container.append(canvas);
-                    const displaySize = {width: video.offsetWidth, height: video.offsetHeight};
-                    faceapi.matchDimensions(canvas, displaySize);
                     setInterval(async () => {
+                        if (canvas) canvas.remove();
+                        canvas = faceapi.createCanvasFromMedia(video);
+                        container.append(canvas);
+                        const displaySize = {width: video.offsetWidth, height: video.offsetHeight};
+                        faceapi.matchDimensions(canvas, displaySize);
                         const singleResult = await faceapi
                             .detectSingleFace(video)
                             .withFaceLandmarks()
@@ -55,11 +56,11 @@
 
                         if (singleResult) {
                             const bestMatch = faceMatcher.findBestMatch(singleResult.descriptor);
-                            console.log(bestMatch.toString())
-                        }
-                        if (singleResult) {
-                            const bestMatch = faceMatcher.findBestMatch(singleResult.descriptor);
-                            console.log(bestMatch.toString())
+                            console.log(bestMatch.toString());
+                            const resizedDetection = faceapi.resizeResults(singleResult, displaySize);
+                            const box = resizedDetection.detection.box;
+                            const drawBox = new faceapi.draw.DrawBox(box, {label: bestMatch.toString()});
+                            drawBox.draw(canvas);
                         }
                         // if (detection){
                         //     const resizedDetection = faceapi.resizeResults(detection, displaySize);
@@ -92,66 +93,7 @@
             })
         }
     }
-    // Prefer camera resolution nearest to 1280x720.
-    //import * as faceapi from "../assets/face-api.min";
-
-    // implements nodejs wrappers for HTMLCanvasElement, HTMLImageElement, ImageData
-    //import * as canvas from 'canvas';
-
     import * as faceapi from 'face-api.js';
-
-    // patch nodejs environment, we need to provide an implementation of
-    // HTMLCanvasElement and HTMLImageElement, additionally an implementation
-    // of ImageData is required, in case you want to use the MTCNN
-    //const { Canvas, Image, ImageData } = canvas
-    //faceapi.env.monkeyPatch({ Canvas, Image, ImageData })
-
-
-    // Promise.all([
-    //     faceapi.nets.tinyFaceDetector.loadFromUri('/models'),
-    //     faceapi.nets.faceLandmark68Net.loadFromUri('/models'),
-    //     faceapi.nets.faceRecognitionNet.loadFromUri('/models'),
-    //     faceapi.nets.faceExpressionNet.loadFromUri('/models')
-    // ]).then(startLiveVideo);
-    //
-    // function startLiveVideo() {
-    //     const constraints = {audio: false, video: {width: 1280, height: 720}};
-    //
-    //
-    //     navigator.mediaDevices.getUserMedia(constraints)
-    //         .then(function(mediaStream) {
-    //             // Prefer camera resolution nearest to 1280x720.
-    //             videoFeed.srcObject = mediaStream;
-    //             videoFeed.onloadedmetadata = function() {
-    //                 videoFeed.play();
-    //             };
-    //
-    //             //I DONT LIKE THIS HERE
-    //             videoFeed.addEventListener('play', () => {
-    //                 // Prefer camera resolution nearest to 1280x720.
-    //                 const videoFeed = document.getElementById('video');
-    //                 var videoParentDiv = document.getElementById('mainComponent');
-    //
-    //                 const canvas = faceapi.createCanvasFromMedia(videoFeed);
-    //                 //document.body.append(canvas);
-    //                 videoFeed.videoParentDiv.insertBefore(canvas, videoFeed.nextSibling);
-    //                 //NEED TO FIGURE OUT DYNAMIC HEIGHT AND WIDTH
-    //                 const displaySize = { width: videoFeed.width, height: videoFeed.height };
-    //                 faceapi.matchDimensions(canvas, displaySize);
-    //                 setInterval(async () => {
-    //                     const detections = await faceapi.detectSingleFace(videoFeed, new faceapi.TinyFaceDetectorOptions()).withFaceLandmarks().withFaceExpressions();
-    //                     if (detections) {
-    //                         const resizedDetections = faceapi.resizeResults(detections, displaySize);
-    //                         canvas.getContext('2d').clearRect(0, 0, canvas.width, canvas.height);
-    //                         faceapi.draw.drawDetections(canvas, resizedDetections);
-    //                         faceapi.draw.drawFaceLandmarks(canvas, resizedDetections);
-    //                         faceapi.draw.drawFaceExpressions(canvas, resizedDetections);
-    //                     }
-    //                 }, 300)
-    //             });
-    //         })
-    //         .catch(function(err) { console.log(err.name + ": " + err.message); });
-    // }
 
 </script>
 
@@ -173,7 +115,7 @@
         left: 0;
         height:100%;
         width:100%;
-        position: absolute;
+        position: relative;
         overflow: hidden;
         z-index: 1;
     }
@@ -183,6 +125,11 @@
         min-width: 100%;
         min-height: 100%;
         object-fit: cover;
+    }
+
+    canvas {
+        position: absolute;
+        z-index: 2;
     }
 
     * {
